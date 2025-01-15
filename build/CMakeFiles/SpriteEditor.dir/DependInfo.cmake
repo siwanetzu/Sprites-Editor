482a656ed33be2e8f8ffb1664635f52834cd02d2
Your Name <your.email@example.com>
2025-01-15 01:57:32 +0000

    Auto-commit: 2025-01-15 01:57:32

---
+++ build/CMakeFiles/SpriteEditor.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "SpriteEditor_autogen/timestamp" "custom" "SpriteEditor_autogen/deps"
+  "/Users/dominikgolab/Documents/GitHub/Sprites Editor/build/SpriteEditor_autogen/mocs_compilation.cpp" "CMakeFiles/SpriteEditor.dir/SpriteEditor_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/SpriteEditor.dir/SpriteEditor_autogen/mocs_compilation.cpp.o.d"
+  "/Users/dominikgolab/Documents/GitHub/Sprites Editor/src/main.cpp" "CMakeFiles/SpriteEditor.dir/src/main.cpp.o" "gcc" "CMakeFiles/SpriteEditor.dir/src/main.cpp.o.d"
+  "/Users/dominikgolab/Documents/GitHub/Sprites Editor/src/mainwindow.cpp" "CMakeFiles/SpriteEditor.dir/src/mainwindow.cpp.o" "gcc" "CMakeFiles/SpriteEditor.dir/src/mainwindow.cpp.o.d"
+  "/Users/dominikgolab/Documents/GitHub/Sprites Editor/src/pakreader.cpp" "CMakeFiles/SpriteEditor.dir/src/pakreader.cpp.o" "gcc" "CMakeFiles/SpriteEditor.dir/src/pakreader.cpp.o.d"
+  "/Users/dominikgolab/Documents/GitHub/Sprites Editor/src/spritemodel.cpp" "CMakeFiles/SpriteEditor.dir/src/spritemodel.cpp.o" "gcc" "CMakeFiles/SpriteEditor.dir/src/spritemodel.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
